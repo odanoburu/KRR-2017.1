5e94b34fcd3c2021208fe6d285adf376543019e7
Alexandre Rademaker <arademaker@gmail.com>
2017-03-23 11:04:15 -0300

    move file and complete the ex 4 cap 3

---
+++ exercises/cap3-ex4.lisp
@@ -0,0 +1,27 @@
+
+(assert '(and
+	  (=> (truth_teller henri) (not (truth_teller pierre)))
+	  (=> (truth_teller pierre) (not (truth_teller henri)))
+	  (or (truth_teller henri) (truth_teller pierre))))
+
+(assert '(forall (?X ?Q)
+	  (iff
+	   (answer_yes ?X ?Q)
+	   (or
+	    (and (truth_teller ?X) (true ?Q))
+	    (and (not (truth_teller ?X)) (not (true ?Q)))))))
+
+(assert '(iff (true gauche) go_left))
+
+(assert '(forall (?X ?Q)
+	  (iff
+	   (true (dit_oui ?X ?Q))
+	   (answer_yes ?X ?Q))))
+
+(assert '(forall (?X ?Q)
+	  (iff
+	   (true (dit_non ?X ?Q))
+	   (not (answer_yes ?X ?Q)))))
+
+(prove '(iff (answer_yes henri ?Q) go_left) :answer '(ans ?Q))
+
---
+++ exercises/philosophers.lisp
@@ -0,0 +1,9 @@
+
+(assert '(philosopher socrates))
+
+(assert '(philosopher plato))
+
+(assert '(philosopher hume))
+
+(prove '(philosopher ?X) :answer '(ans ?X))
+
