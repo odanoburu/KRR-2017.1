fa63406ae7f6720d5c1bb10356c468e5a86a6378
odanoburu <bcclaro@gmail.com>
2017-06-06 19:32:14 -0300

    begin changes in structure after class presentation

--- exercises/sld.lisp
+++ exercises/sld.lisp
@@ -12,12 +12,12 @@
 ;4. otherwise, return NO
 
 (defparameter *child-KB*
-  '((Toddler)
+  (copy-tree '((Toddler)
     ((not Toddler) Child)
     ((not Child) (not Male) Boy)
     ((not Infant) Child)
     ((not Child) (not Female) Girl)
-    (Female))
+    (Female)))
   "A simple KB.")
 
 (defvar *KB* *child-KB*
@@ -36,51 +36,48 @@
 (setf *CONCLUSIONS*
       nil)
 
-(setf *GOALS*
-      nil)
-
-(defun add-atom(atom ix)
+(defun add-atom(atom ix atoms)
   "pushes an empty atom record to the ATOMS variable, except for its on-clauses property, which has the clause where it was found."
-  (push (list :atom atom :visited nil :on-clauses (list ix)) *ATOMS*))
+  (push (list :atom atom :visited nil :on-clauses (list ix)) atoms))
 
-(defun update-atom(atom ix)
+(defun update-atom(atom ix atoms)
   "adds to the atom's on-clauses property another clause where it was found."
   (let ((atomo (find-atom atom)))
-    (setf (getf atomo :on-clauses) (cons ix (get-indices atom)))))
+    (setf (getf atomo :on-clauses) (cons ix (get-indices atom atoms)))))
 
-(defun find-atom(atom)
+(defun find-atom(atom atoms)
   "returns the property list of a given atom"
-  (find atom *atoms* :key (lambda (record) (getf record :atom))))
+  (find atom atoms :key (lambda (record) (getf record :atom))))
 
 ;(defun remove-atom(atom)
 ;  "returns the property list of all atoms except for the one provided."
 ;  (remove-if-not #'(lambda(record) (not (equal (getf record :atom) atom))) *ATOMS*))
 
-(defun get-indices(atom)
+(defun get-indices(atom atoms)
   "gets indices of clauses where atom is found."
-  (getf (find-atom atom) :on-clauses))
+  (getf (find-atom atom atoms) :on-clauses))
 
-(defun make-atoms(clause ix)
+(defun make-atoms(clause ix atoms)
   "takes a clause and recursively builds the ATOMS variable."
-  (cond ((null clause) *ATOMS*)
-	(t (if (null (find-atom (first clause)))
-	    (add-atom (first clause) ix); if atom not in ATOMS
-	    (update-atom (first clause) ix)); if atom already in ATOMS
-	 (make-atoms (rest clause) ix)))); recurse
+  (cond ((null clause) atoms)
+	(t (if (null (find-atom (first clause) atoms))
+	    (add-atom (first clause) ix atoms); if atom not in ATOMS
+	    (update-atom (first clause) ix atoms)); if atom already in ATOMS
+	 (make-atoms (rest clause) ix atoms)))); recurse
 
-(defun make-conclude(clause ix)
+(defun make-conclude(clause ix clauses)
   "make the plist conclusion, where the property name is the atom/goal and the value is the number of atoms appearing negatively in the clause that are not yet known to be true."
-  (push (cons ix (list (first (last clause)) (- (length clause) 1))) *CONCLUSIONS*))
+  (push (cons ix (list (first (last clause)) (- (length clause) 1))) clauses))
 
-(defun parse-kb(kb)
+(defun parse-kb(kb atoms conclusions)
   "parses KB in appropriate format"
   (loop for clause in kb
      for ix from 0
-     do (make-atoms clause ix)
-     do (make-conclude clause ix)))
+     do (make-atoms clause ix atoms)
+     do (make-conclude clause ix conclusions)))
 
-(defun add-to-goals(atom &optional (solved nil))
-  (push (list atom solved) *GOALS*))
+(defun add-to-goals(atom goals &optional (solved nil))
+  (push (list atom solved) goals))
 
 ;(defun query-kb(query kb)
 ;  (add-to-goals query)))
